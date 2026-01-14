41d03b140f34ac6c976c66b0cd64a501dcccae94
PeilinZHENG <phylincszheng@gmail.com>
2026-01-14 11:54:31 +0800

    Update .gitignore and configuration files; enhance si1000 noise model with correct parameter

--- simulator/si1000_generator.py
+++ simulator/si1000_generator.py
@@ -1,4 +1,8 @@
 import stim
+from typing import List
+
+def _targets_list(inst) -> List[int]:
+    return [t.value for t in inst.targets_copy()]
 
 
 def si1000_noise_model(config: dict) -> stim.Circuit:
@@ -17,13 +21,37 @@ def si1000_noise_model(config: dict) -> stim.Circuit:
     p = config.get("p", 0.001)
     distance = config.get("distance", 3)
     rounds = config.get("rounds", 25)
-    circuit = stim.Circuit.generated(
-        "surface_code:rotated_memory_z",
+    basis = config.get("basis", "z").lower()
+    # circuit = stim.Circuit.generated(
+    #     "surface_code:rotated_memory_z",
+    #     rounds=rounds,
+    #     distance=distance,
+    #     after_clifford_depolarization=p,          # p for 2Q gates (DEPOLARIZE2)
+    #     before_round_data_depolarization=p / 10,  # p/10 for idle
+    #     before_measure_flip_probability=5 * p,    # 5p for measurement
+    #     after_reset_flip_probability=2 * p,       # 2p for reset (SI1000 spec)
+    # )
+
+    ideal = stim.Circuit.generated(
+        f"surface_code:rotated_memory_{basis}",
         rounds=rounds,
         distance=distance,
-        after_clifford_depolarization=p,          # p for 2Q gates (DEPOLARIZE2)
         before_round_data_depolarization=p / 10,  # p/10 for idle
         before_measure_flip_probability=5 * p,    # 5p for measurement
         after_reset_flip_probability=2 * p,       # 2p for reset (SI1000 spec)
-    )
-    return circuit
+    ).flattened()
+
+    noisy = stim.Circuit()
+    for inst in ideal:
+        name = inst.name
+        tgts = _targets_list(inst)
+
+        noisy.append(inst)
+
+        if name in ("H", "S", "S_DAG"):  # p/10 for 1Q gates (DEPOLARIZE1)
+            noisy.append_operation("DEPOLARIZE1", tgts, p / 10)
+
+        elif name in ("CX", "CZ"):  # p for 2Q gates (DEPOLARIZE2)
+            noisy.append_operation("DEPOLARIZE2", tgts, p)
+
+    return noisy
